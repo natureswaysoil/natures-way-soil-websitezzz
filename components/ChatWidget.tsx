bc333dba0b13a72182fded3e4aff4956e0b3c922
natureswaysoil <natureswaysoil@gmail.com>
2025-08-10 16:55:30 -0400

    Update ChatWidget.tsx

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,1 +1,6 @@
-export { default } from './ChatWidgetStreaming';
+import dynamic from 'next/dynamic';
+
+// Load the client-only streaming widget on the client side only
+const ChatWidget = dynamic(() => import('./ChatWidgetStreaming'), { ssr: false });
+
+export default ChatWidget;
